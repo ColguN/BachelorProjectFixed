6634e61ee915484150a65a061af3205f62d070cf
ColguN <can.olgun@outlook.com>
2024-04-25 17:22:01 +0200

    Update test_webscraping.py
    fixed issue where it wouldnt load more ideas if it hadn't collected 10 ideas already

--- test_webscraping.py
+++ test_webscraping.py
@@ -31,6 +31,21 @@ for trader in traders:
         # Locate all ideas on the page at this moment
         ideas = driver.find_elements(By.CSS_SELECTOR,
                                      'div.tv-card-container > div.tv-card-container__columns > div.js-card-list.tv-card-container__ideas > div.tv-feed__item.tv-feed-layout__card-item.js-feed__item--inited')
+        if counter_ideas >= len(ideas):
+            # click the load more button at the bottom of the ideas
+            try:
+                load_more_button = WebDriverWait(driver, 5).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.tv-load-more__btn.js-load-more.tv-button--loader'))
+                )
+                load_more_button.click()
+                # Wait for new ideas to load
+                time.sleep(5)
+                # Refresh the list of ideas
+                ideas = driver.find_elements(By.CSS_SELECTOR,
+                                             'div.js-card-list.tv-card-container__ideas > div.tv-feed__item.tv-feed-layout__card-item.js-feed__item--inited')
+            except Exception as e:
+                print("No more ideas to load or failed to load more ideas:", str(e))
+                break
 
         # Click on current Idea and wait until everything is loaded
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable(ideas[counter_ideas])).click()
